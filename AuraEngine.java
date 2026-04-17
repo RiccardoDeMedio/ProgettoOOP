169d54d01abb63885de9e2a5cd090cb7ab436ad2
Federico Giorgetti <federico.giorgetti2@studio.unibo.it>
2026-04-17 16:52:47 +0200

    AuraEngine.java updated, added base parameters and Constructor

--- AuraEngine.java
+++ AuraEngine.java
@@ -1,3 +1,11 @@
 public class AuraEngine {
-    
+    private final int maxDepth;
+    private final int squares = 64;
+
+    public AuraEngine(int maxDepth) {
+        this.maxDepth = maxDepth;
+    }
+    public int getDepth() {
+        return maxDepth;
+    }
 }
