558ff88f3bfbc0d8af1b4aa8880a066f98f6be4c
Federico Giorgetti <federico.giorgetti2@studio.unibo.it>
2026-05-21 13:59:34 +0200

    Created pieceTable with the pawn value

--- AuraEngine.java
+++ AuraEngine.java
@@ -1,6 +1,28 @@
 public class AuraEngine {
     private final int maxDepth;
     private final int squares = 64;
+    private final int pieceTable[][] = { // To assign a value to each piece for each square
+        { // White Pawn
+            0,   0,   0,   0,   0,   0,   0,   0,   // 8 - Impossible for pawns
+            50,  50,  50,  50,  50,  50,  50,  50,  // 7
+            10,  10,  20,  30,  30,  20,  10,  10,  // 6
+            5,   5,  10,  25,  25,  10,   5,   5,   // 5
+            0,   0,   0,  20,  20,   0,   0,   0,   // 4
+            5,  -5, -10,   0,   0, -10,  -5,   5,   // 3
+            5,  10,  10, -20, -20,  10,  10,   5,   // 2 
+            0,   0,   0,   0,   0,   0,   0,   0    // 1 - Impossible for pawns
+        },
+        { // Black Pawn
+            0,   0,   0,   0,   0,   0,   0,   0,   // 1 - Impossible for pawns
+            5,  10,  10, -20, -20,  10,  10,   5,   // 2
+            5,  -5, -10,   0,   0, -10,  -5,   5,   // 3
+            0,   0,   0,  20,  20,   0,   0,   0,   // 4
+            5,   5,  10,  25,  25,  10,   5,   5,   // 5
+            10,  10,  20,  30,  30,  20,  10,  10,  // 6
+            50,  50,  50,  50,  50,  50,  50,  50,  // 7
+            0,   0,   0,   0,   0,   0,   0,   0,   // 8 - Impossible for pawns
+        },
+    };
     private final int endTableSpots = 2; // To avoid the presence of magic numbers, we define the spot in the table for Kings.
 
     public AuraEngine(int maxDepth) {
@@ -9,7 +31,6 @@ public class AuraEngine {
     public int getDepth() {
         return maxDepth;
     }
-    
 
     private int evaluateBoard(Board board) {
         int totalScore = 0;
